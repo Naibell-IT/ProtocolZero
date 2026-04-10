a5506e792538d45054ce413fd46dc621f5f43578
Насонов Михаил <111873540+Naibell-IT@users.noreply.github.com>
2026-04-10 21:47:30 +0400

    Изменения в исходном коде прошлого коммита

--- Source/ProtocolZero/MainCharacter/MainCharacter.cpp
+++ Source/ProtocolZero/MainCharacter/MainCharacter.cpp
@@ -5,6 +5,13 @@
 
 AMainCharacter::AMainCharacter()
 {
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
+	Camera->SetupAttachment(GetCapsuleComponent());
+	Camera->SetRelativeLocation(FVector(0, 0, 60));
+	Camera->bUsePawnControlRotation = true;
+
+	GetCharacterMovement()->MaxWalkSpeed = DefaultWalkSpeed;
+
 	PrimaryActorTick.bCanEverTick = true;
 
 }
@@ -13,6 +20,11 @@ void AMainCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (APlayerController* PC = Cast<APlayerController>(GetController())) {
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer())) {
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
 }
 
 void AMainCharacter::Tick(float DeltaTime)
@@ -24,6 +36,38 @@ void AMainCharacter::Tick(float DeltaTime)
 void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMainCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AMainCharacter::Look);
+	}
 
+}
+
+void AMainCharacter::Move(const FInputActionValue& Value)
+{
+	FVector2D move_vector = Value.Get<FVector2D>();
+
+	if (Controller)
+	{
+		const FRotator rotation = Controller->GetControlRotation();
+		const FRotator yaw_rotation(0, rotation.Yaw, 0);
+		const FVector forward_direction = FRotationMatrix(yaw_rotation).GetUnitAxis(EAxis::X);
+		const FVector right_direction = FRotationMatrix(yaw_rotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(forward_direction, move_vector.Y);
+		AddMovementInput(right_direction, move_vector.X);
+	}
+}
+
+void AMainCharacter::Look(const FInputActionValue& Value)
+{
+	FVector2D look_vector = Value.Get<FVector2D>();
+
+	if (Controller)
+	{
+		AddControllerYawInput(look_vector.X);
+		AddControllerPitchInput(look_vector.Y);
+	}
 }
 
--- Source/ProtocolZero/MainCharacter/MainCharacter.h
+++ Source/ProtocolZero/MainCharacter/MainCharacter.h
@@ -4,7 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
 #include "MainCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class PROTOCOLZERO_API AMainCharacter : public ACharacter
@@ -21,5 +30,22 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputMappingContext* DefaultMappingContext;
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* MoveAction;
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* LookAction;
+public:
+	UPROPERTY(EditDefaultsOnly)
+	UCameraComponent* Camera;
+public:
+	UFUNCTION(BlueprintCallable)
+	void Move(const FInputActionValue& Value);
+	UFUNCTION(BlueprintCallable)
+	void Look(const FInputActionValue& Value);
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Movement Settings")
+	float DefaultWalkSpeed = 500;
 };
